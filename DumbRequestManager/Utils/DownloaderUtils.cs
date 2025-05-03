ba801dc7f78cbe7150e1ef59042dc8830c92f981
TheBlackParrot <theblackparrot0@gmail.com>
2025-05-03 00:19:57 -0500

    drop dependency on BeatSaverDownloader, we do everything of the downloading now

--- DumbRequestManager/Utils/DownloaderUtils.cs
+++ DumbRequestManager/Utils/DownloaderUtils.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using BeatSaverDownloader.Misc;
 using BeatSaverSharp.Models;
 using JetBrains.Annotations;
 using SiraUtil.Web;
@@ -53,8 +54,44 @@ internal class DownloaderUtils(IHttpService httpService) : IInitializable
             Plugin.Log.Warn("[DownloadUtils] Map download was empty");
             return;
         }
+
+        string folderName = $"{beatmap.ID} ({beatmap.Metadata.SongName} - {beatmap.Metadata.LevelAuthorName})";
+        Plugin.DebugMessage($"[DownloadUtils] Folder name before: {folderName}...");
+        Path.GetInvalidFileNameChars().Concat(Path.GetInvalidPathChars()).Aggregate(folderName,
+            (current, invalidCharacter) => current.Replace(invalidCharacter.ToString(), string.Empty));
+        Plugin.DebugMessage($"[DownloadUtils] Folder name after: {folderName}...");
         
-        await SongDownloader.ExtractZipAsync(beatmap, result, customSongsPath);
+        string fullFolderPath = Path.Combine(customSongsPath, folderName);
+        if (!Directory.Exists(fullFolderPath))
+        {
+            Plugin.DebugMessage("[DownloadUtils] Folder doesn't exist, making it");
+            Directory.CreateDirectory(fullFolderPath);
+        }
+        else
+        {
+            Plugin.DebugMessage("[DownloadUtils] Folder exists, making a dupe");
+            
+            string originalFullFolderPath = fullFolderPath;
+            for (int dupes = 1; Directory.Exists(fullFolderPath); dupes++)
+            {
+                fullFolderPath = originalFullFolderPath + $" ({dupes++})";
+            }
+            
+            Plugin.DebugMessage($"[DownloadUtils] Dupe folder name: {fullFolderPath}");
+            Directory.CreateDirectory(fullFolderPath);
+        }
+        
+        MemoryStream stream = new(result);
+        Plugin.DebugMessage("[DownloadUtils] Created MemoryStream");
+        ZipArchive archive = new(stream, ZipArchiveMode.Read);
+        Plugin.DebugMessage("[DownloadUtils] Created ZipArchive");
+        archive.ExtractToDirectory(fullFolderPath);
+        Plugin.DebugMessage($"[DownloadUtils] Extracted to {fullFolderPath}");
+        
+        await stream.DisposeAsync();
+        archive.Dispose();
+        Plugin.DebugMessage("[DownloadUtils] Disposed stuff");
+        
         SongCore.Loader.Instance.RefreshSongs(false);
     }
 }
