8cbf1cd6cf32a37bb53e17cebc156368eda300da
TheBlackParrot <theblackparrot0@gmail.com>
2025-06-03 06:54:38 -0500

    bit o cleanup

--- DumbRequestManager/UI/SettingsMenuManager.cs
+++ DumbRequestManager/UI/SettingsMenuManager.cs
@@ -35,20 +35,12 @@ internal class SettingsMenuManager : IInitializable, IDisposable
     
     public void Initialize()
     {
-#if V1_39_1
         BeatSaberMarkupLanguage.Settings.BSMLSettings.Instance.AddSettingsMenu("DumbRequestManager",
             "DumbRequestManager.UI.BSML.Settings.bsml", this);
-#else
-        BeatSaberMarkupLanguage.Settings.BSMLSettings.instance?.AddSettingsMenu("DumbRequestManager", "DumbRequestManager.UI.BSML.Settings.bsml", this);
-#endif
     }
 
     public void Dispose()
     {
-#if V1_39_1
         BeatSaberMarkupLanguage.Settings.BSMLSettings.Instance?.RemoveSettingsMenu(this);
-#else
-        BeatSaberMarkupLanguage.Settings.BSMLSettings.instance?.RemoveSettingsMenu(this);
-#endif
     }
 }
