d91a28a53760242562c05685e1cc9e60a584822d
ポッチャマ <mercworld@pocha.moe>
2025-12-10 23:15:28 -0500

    refactor: wait did i move these. probably.

---
+++ DumbRequestManager/Services/API/Models/SocketMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace DumbRequestManager.Services.API.Models;
+
+[JsonObject]
+public class APIMessage(string eventName, object? data)
+{
+    public long Timestamp => DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
+    // [TODO] aw fuck this is a breaking change
+    public string Event = eventName;
+    public object? Data = data;
+
+    public override string ToString()
+    {
+        return JsonConvert.SerializeObject(this);
+    }
+}
+
+[JsonObject]
+public class HookMessage(string eventName, object? data) : APIMessage(eventName, data)
+{
+    [JsonProperty("id")] private string _id = Guid.NewGuid().ToString();
+
+    public override string ToString()
+    {
+        return JsonConvert.SerializeObject(this, Formatting.Indented, new JsonSerializerSettings{
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        });
+    }
+}
